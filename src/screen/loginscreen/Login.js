0a98b5d43bd0da87c82cdae59725381f273aa33f
tiodata <elmitodatacp@gmail.com>
2023-09-21 14:19:59 +0700

    MENGUBAH UI

--- src/screen/loginscreen/Login.js
+++ src/screen/loginscreen/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Logounikama from '../../../assets/images/logo.png';
+import Logounikama from '../../../assets/logo.png';
 import { View, Text, TextInput, Button, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import Latar from '../../../assets/latar.png';
 
@@ -31,13 +31,15 @@ const Login = ({navigation}) => {
             <Image source={Logounikama} style={styles.unikama} />
 
             <Text style={styles.unikamaText}>
-                Aplikasi <Text style={styles.blueText}>Unggah Mandiri</Text>Karya Ilmiah
+                Aplikasi <Text style={styles.blueText}> Unggah  Mandiri </Text> Karya Ilmiah
             </Text>
 
 
             {/* Isi halaman login */}
             <View style={styles.loginContent}>
                 <Text selectable={false} style={styles.header}>Sign in to</Text>
+                <View style={styles.inpcontainer}>
+
                 <TextInput
                     placeholder="Username"
                     value={username}
@@ -51,7 +53,8 @@ const Login = ({navigation}) => {
                     secureTextEntry
                     style={styles.input}
                 />
-                <Button title="Sign ini" onPress={handleLogin} />
+                </View>
+                <Button title="Sign in" onPress={handleLogin} style={styles.tombolmasuk} />
                 <TouchableOpacity onPress={handleHelp}>
                     <Text style={styles.bantuan}>Bantuan</Text>
                 </TouchableOpacity>
@@ -73,47 +76,56 @@ const styles = StyleSheet.create({
         position: 'absolute',
         width: '100%',
         height: '100%',
+        backgroundColor: 'rgba(128, 128, 128, 0.5)', // Warna abu-abu dengan transparansi
     },
     loginContent: {
         backgroundColor: 'rgba(255, 255, 255, 0.7)',
-        padding: 20,
+        padding: 27,
         borderRadius: 10,
         alignItems: 'center',
-        width: '75%',
+        width: '80%',
+        marginTop: 55,
         justifyContent: 'center',
         flexDirection: 'column',
-        height: '40%', // Atur tinggi sesuai kebutuhan Anda, misalnya '60%'
+        height: '55%', // Atur tinggi sesuai kebutuhan Anda, misalnya '60%'
     },
     header: {
         fontSize: 24,
+        paddingTop: 10,
         marginBottom: 20,
         color: '#3435AA',
         fontWeight: 'bold', // Set fontWeight to 'bold'
     },
     input: {
-        width: '80%',
-        height: 40,
-        borderWidth: 1,
-        marginBottom: 10,
-        padding: 10,
-        fontWeight: '500',
+        width: '90%',
+        height: 45,
+        borderWidth: 2,
+        marginBottom: 15,
+        padding: 9,
+        fontWeight: '600',
         backgroundColor: '#C0C0C0',
         fontSize: 12,
+        
     },
     unikama: {
         position: 'absolute',
-        top: '2%',
-        width: '30%',
-        height: '20%',
-        backgroundColor: 'transparent',
+        top: '1%',
+        maxWidth: '',
+        width: 100,
+        height: 100,
+        marginTop: 0,
+        // backgroundColor: 'transparent',
         zIndex: 1,
     },
     /* Gaya untuk teks aplikasi dan mandiri */
     unikamaText: {
-        color: '#000000',// unggah mandiri
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Ganti warna dan opacity sesuai keinginan Anda
+        color: '#FFFFFF', // Warna teks
         position: 'absolute',
-        top: '20%',
-        fontSize: 17,
+        top: '18%',
+        fontSize: 22,
+        padding: 8, // Atur padding sesuai kebutuhan Anda
+        borderRadius: 9,
     },
     blueText: {
         color: 'blue', //karya ilmiah
@@ -122,10 +134,19 @@ const styles = StyleSheet.create({
 
     bantuan: {
         color: 'blue',
-        fontSize: 16,
+        fontSize: 20,
         textDecorationLine: 'underline',
-    },
+        marginTop: 15,
 
+
+    },
+    tombolmasuk:{
+        paddingTop:20,
+    },
+    inpcontainer: {
+        width:'100%',
+        marginBottom: 17,
+    },
 
 
 
