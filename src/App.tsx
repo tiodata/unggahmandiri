17e5f78fe61c455c70e13c00647949cee5907c28
Daiita30 <davidcristianps675@gmail.com>
2023-10-17 11:22:48 +0700

    add back hardware=exitapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
-///App.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { Alert, BackHandler } from 'react-native';
 import AuthNavigator from './navigation/AuthNavigator.js';
 
-
 export default function App() {
-  // isAuthenticated = is...
+  const [isExitPromptVisible, setExitPromptVisible] = useState(false);
+
+  useEffect(() => {
+    const backAction = () => {
+      if (isExitPromptVisible) {
+        // If the exit prompt is already visible, exit the app
+        BackHandler.exitApp();
+        return true;
+      } else {
+        // Show the exit prompt
+        setExitPromptVisible(true);
+        setTimeout(() => {
+          setExitPromptVisible(false);
+        }, 2000); // Reset the prompt after 2 seconds
+        return true; // Prevent default back action
+      }
+    };
+
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+
+    return () => backHandler.remove();
+  }, [isExitPromptVisible]);
+
   return (
     <NavigationContainer>
-
-      {/* {isAuthenticated ? AuthNavigator : DrawerNavigator } */}
       <AuthNavigator />
-
     </NavigationContainer>
   );
 }
